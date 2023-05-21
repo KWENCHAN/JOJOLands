4d9d3a437a2fe3ffdcbed72a642f7883c7713095
Kaiwen <chankaiwen2003@gmail.com>
2023-05-22 03:07:39 +0800

    Create Graph, Map, TheWorld class

---
+++ Graph/Edge.java
@@ -0,0 +1,31 @@
+package Graph;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author ASUS
+ */
+public class Edge {
+    Location destination;
+    int weight;
+
+    public Edge(Location tovertex, int weight) {
+        this.destination = tovertex;
+        this.weight = weight;
+    }
+
+    public Location getTovertex() {
+        return destination;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+    
+    
+}
---
+++ Graph/Location.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Graph;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Location {
+
+    private String name;
+
+    public Location(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    protected void displayLocationOptions(Graph map) {
+
+        ArrayList<Edge> edges = map.getEdge(this);
+
+        if (edges.isEmpty()) {
+            System.out.println("There are no available destinations from this location.");
+            return;
+        }
+
+        char option = 'A';
+
+        for (Edge edge : edges) {
+            Location destination = edge.getTovertex();
+            System.out.print("[" + option + "] " + destination.getName()+"\t");
+            option++;
+        }
+    }
+    
+}
