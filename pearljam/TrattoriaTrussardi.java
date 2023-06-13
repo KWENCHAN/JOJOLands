8cb5b0dad89f6128644091a0cbc8bf17185b2df1
Chan Kai Wen <chankaiwen2003@gmail.com>
2023-06-14 00:05:23 +0800

    Connected TrattoriaTrussardi and GreenDolphinStreetPrison

--- pearljam/TrattoriaTrussardi.java
+++ pearljam/TrattoriaTrussardi.java
@@ -8,7 +8,10 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-public class TrattoriaTrussardi extends PearlJam {
+import JOJOLands.Action;
+import JOJOLands.TheWorld;
+
+public class TrattoriaTrussardi extends PearlJam implements Action{
 
     public TrattoriaTrussardi() {
         super("Trattoria Trussardi");
@@ -16,6 +19,68 @@ public class TrattoriaTrussardi extends PearlJam {
         addToMenu("Creme caramel", 6.50);
         addToMenu("Lamb Chops with Apple Sauce", 25.00);
         addToMenu("Spaghetti alla Puttanesca", 15.00);
+    }
+
+    public void action(TheWorld game) {
+        displayMenu(game);
+        String select = game.getSelection();
+        System.out.println("=".repeat(70));
+        switch (select.charAt(0)) {
+            case '1':
+                char loc_select = select.charAt(1);
+                if ((loc_select - 'A' <= game.getMap().getEdge(game.getCurrentLocation()).size() - 1)
+                        && Character.isUpperCase(loc_select)) {
+                    game.move(loc_select);
+                } else {
+                    System.out.println("Option " + select + " is not available. Please reselect.");
+                    action(game);
+                }
+                break;
+            case '2':
+                System.out.println("Restaurant: " + getName() + "\n");
+                displayWaitingList();
+                displayOrderProcessingList();
+                break;
+            case '3':
+                viewMenu();
+                break;
+            case '4':
+                // view sales info
+                break;
+            case '5':
+                // Milagro Man
+                break;
+            case '6':
+                if (!game.getBackhistory().isEmpty()) {
+                    game.back();
+                } else if (game.getBackhistory().isEmpty() && !game.getForwardhistory().isEmpty()) {
+                    game.forward();
+                } else if (game.getBackhistory().isEmpty() && game.getForwardhistory().isEmpty()) {
+                    game.backToTownHall();
+                } else {
+                    System.out.println("Option " + select + " is not available. Please reselect.");
+                }
+                break;
+            case '7':
+                if (!game.getBackhistory().isEmpty() && !game.getForwardhistory().isEmpty()) {
+                    game.forward();
+                } else if ((game.getBackhistory().isEmpty() && !game.getForwardhistory().isEmpty())
+                        || (!game.getBackhistory().isEmpty()) && game.getForwardhistory().isEmpty()) {
+                    game.backToTownHall();
+                } else {
+                    System.out.println("Option " + select + " is not available. Please reselect.");
+                }
+                break;
+            case '8':
+                if (!game.getBackhistory().isEmpty() && !game.getForwardhistory().isEmpty()) {
+                    game.backToTownHall();
+                } else {
+                    System.out.println("Option " + select + " is not available. Please reselect.");
+                }
+                break;
+            default:
+                System.out.println("Option " + select + " is not available. Please reselect.");
+        }
     }
 
     public void processOrdersTrattoriaTrussardi() {
@@ -50,5 +115,22 @@ public class TrattoriaTrussardi extends PearlJam {
         orderProcessingList.addAll(males);
         orderProcessingList.addAll(females);
         orderProcessingList.addAll(unspecified);
+    }
+
+    public static void displayMenu(TheWorld game) {
+        int i = 2;
+        System.out.println("Current Location: " + game.getCurrentLocation().getName());
+        game.displayCurrentLocationOptions();
+        System.out.printf("[%d] View Waiting List and Order Processing List%n", i++);
+        System.out.printf("[%d] View Menu%n", i++);
+        System.out.printf("[%d] View Sales Information%n", i++);
+        System.out.printf("[%d] Milagro Man%n", i++);
+        if (!game.getBackhistory().isEmpty()) {
+            System.out.printf("[%d] Back (%s)%n", i++, game.getBackhistory().peek().getName());
+        }
+        if (!game.getForwardhistory().isEmpty()) {
+            System.out.printf("[%d] Forward (%s)%n", i++, game.getForwardhistory().peek().getName());
+        }
+        System.out.printf("[%d] Back to Town Hall%n%n", i++);
     }
 }
